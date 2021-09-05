a6aaf892b89fe482f4f0c27eefd696ad401570a5
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-09-05 15:00:10 +0300

    Optimize.

--- homework_1_b/3.py
+++ homework_1_b/3.py
@@ -1,17 +1,10 @@
 def is_leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            return True if (year % 400) == 0 else False
-        else:
-            return True
-    else:
-        return False
+    return True if y % 4 != 0 or (y % 100 == 0 and y % 400 != 0) else False
 
 
-def may_be_correct(day, month, year):
+def correct(day, month, year):
     the_31_days = (1, 3, 5, 7, 8, 10, 12)
-
-    if day > 28 and month < 13:
+    if month < 13:
         if month in the_31_days:
             return True
         elif day > 29 and month != 2:
@@ -19,16 +12,10 @@ def may_be_correct(day, month, year):
         elif day == 29 and month == 2:
             return True if is_leap_year(year) else False
         else:
-            return False
-    elif month < 13:
-        return True
+            return True
     else:
         return False
 
 
 x, y, z = map(int, input().split())
-
-first = may_be_correct(x, y, z)
-second = may_be_correct(y, x, z)
-
-print(1 if ((first or second) and first is not second) or y == x else 0)
+print(1 if y == x or correct(x, y, z) is not correct(y, x, z) else 0)
